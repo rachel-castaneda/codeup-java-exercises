dace79c1cc32be20a2e49fc49570b91d5838d533
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-01-27 16:23:52 -0600

    finished helloworld exercise and started console exercise

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,8 @@
+public class ConsoleExercises {
+    public static void main (String[] args) {
+
+        double pi = 3.14159;
+        String piIsPi = "the value of pi is approximately " + pi;
+        System.out.println(piIsPi);
+    }
+}
