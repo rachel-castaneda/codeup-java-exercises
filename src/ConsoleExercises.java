371d6d85fd05ac5ce906a8eae424dc493ebd0481
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-01-27 16:45:41 -0600

    worked console exercises started scanner

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -2,7 +2,8 @@ public class ConsoleExercises {
     public static void main (String[] args) {
 
         double pi = 3.14159;
-        String piIsPi = "the value of pi is approximately " + pi;
-        System.out.println(piIsPi);
+        System.out.print("the value of pi is approximately ");
+        System.out.format("%.2f%n", pi);
+
     }
 }
---
+++ src/Scanner.java
@@ -0,0 +1,12 @@
+public class Scanner {
+    public static void  main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter something: ");
+        String userInput = scanner.next();
+
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+    }
+}
