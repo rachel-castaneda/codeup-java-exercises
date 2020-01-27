8c02481e8af04e06eeb9a378c547c59263348b68
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-01-27 17:00:15 -0600

    worked on scanner

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,7 +1,7 @@
 public class Scanner {
     public static void  main (String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
 
         System.out.print("Enter something: ");
         String userInput = scanner.next();
