b479b73a8f3adf7f950d115f1e0dfdd8d04e4156
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-01-28 18:42:35 -0600

    worked on exercise for statements and loops

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,77 @@
+public class ControlFlowExercises {
+    public static void  main (String[] args){
+
+//        count to 15 starting at 5
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        for (int i =5;i <=15; i++ ){
+//            System.out.println(i);
+//        }
+//        System.out.println("=============");
+
+//        count by 2's from 0-100
+//        int j = 0;
+//        do {
+//            System.out.println(j);
+//            j+=2;
+//        } while (j <= 100);
+
+//          for (int j=0; j<=100; j+=2) {
+//                System.out.println(j);
+//          }
+//        System.out.println("=============");
+//
+//    count down from 100 by 5's
+//        int j = 100;
+//        do {
+//            System.out.println(j);
+//            j-=5;
+//        } while (j >= -10);
+
+//          for (int j=100; j>=-10;j-=5){
+//              System.out.println(j);
+//          }
+//        System.out.println("=============");
+
+//      count by squared iterations from 2 - 1,000,000
+//        long k = 2;
+//        do {
+//            System.out.println(k);
+//            k*=k;
+//
+//        } while (k < 1000000);
+//
+//        for(long l =2; l <1000000; l++){
+//            if (l >= 1000000){
+//                break;
+//            } else {
+//                System.out.println(l);
+//                l*=l;
+//            }
+//        }
+//        System.out.println("=============");
+
+// FizzBuzz
+//        int i;
+//        for (i=1; i<=100; ++i){
+//            if (i %15==0)
+//                System.out.println(i+" : FizzBuzz");
+//            else if ((i%3)==0)
+//                System.out.println(i+" : Fizz");
+//            else if (i % 5 ==0)
+//                System.out.println(i+" : Buzz");
+//            else
+//                System.out.println(i);
+//        }
+
+        System.out.println("enter an integer: ");
+        double num = 0;
+
+
+
+    }
+}
