e6dc78903c74bdfdbb3239b8d9c6f389cbed6d92
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-02-03 09:27:29 -0600

    worked on morning exercise to add two binary numbers

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,13 +1,13 @@
-package shapes;
-
-import util.Input;
-public class CircleApp {
-    public static void  main (String [] args) {
-        Input input = new Input();
-        double radius = input.getDouble("");
-        Circle circle = new Circle(radius);
-        System.out.println(circle.getArea());
-        System.out.println(circle.getCircumference());
+//package shapes;
+//
+//import util.Input;
+//public class CircleApp {
+//    public static void  main (String [] args) {
+//        Input input = new Input();
+//        double radius = input.getDouble("");
+//        Circle circle = new Circle(radius);
+//        System.out.println(circle.getArea());
+//        System.out.println(circle.getCircumference());
 //        System.out.println(circle.radius());
-    }
-}
+//    }
+//}
