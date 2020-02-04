20ad096df351484827c52872e81f5e74c184a41a
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-02-04 14:21:13 -0600

    worked on circles exercise

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,16 +1,25 @@
 package shapes;
 
 public class Circle {
+
+    private static int circlesCreated = 0;
     private double radius;
 
-    public Circle(double radius) {
+    public Circle (double radius) {
         this.radius = radius;
+        circlesCreated++;
     }
-    public double getArea(){
+
+    public double getArea() {
         return Math.PI * this.radius * this.radius;
     }
-    public double getCircumference() {
-        return 2*this.radius*Math.PI;
+
+    public double getCircumference () {
+        return 2 * this.radius * Math.PI;
+    }
+
+    public static int getCirclesCreated() {
+        return circlesCreated;
     }
 
 }
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,13 +1,17 @@
-//package shapes;
-//
-//import util.Input;
-//public class CircleApp {
-//    public static void  main (String [] args) {
-//        Input input = new Input();
-//        double radius = input.getDouble("");
-//        Circle circle = new Circle(radius);
-//        System.out.println(circle.getArea());
-//        System.out.println(circle.getCircumference());
-//        System.out.println(circle.radius());
-//    }
-//}
+package shapes;
+
+import java.util.Input;
+public class CircleApp {
+
+    public static void main (String [] args) {
+        Input input = new Input();
+        do {
+            double radius = input.getDouble("what is the radius of the circle?");
+            Circle circle = new Circle(radius);
+            System.out.println("area of a circle = " + circle.getArea());
+            System.out.println("circumference of a circle = " + circle.getCircumference());
+        } while (input.yesNo("do you want to make another? "));
+        System.out.println(Circle.getCirclesCreated() + "circles created: ");
+    } // main method close
+
+} // class CircleApp close
