2fd510b9ad3b8abf87cf9f785ddb488c99c19e5a
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-02-04 14:51:45 -0600

    worked on server name generator

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,5 +1,20 @@
 public class ServerNameGenerator {
     final public static String [] adjectives = {"pretty", "obscure", "distant", "radical", "meaningful", "sincere", "dubious", "faux", "honest", "dire"};
 
-//    final public static
-}
+    final public static String [] nouns = {"horse", " machine", "despot", "radical", "definitions", "streak", "warning", "observation", "merchant", "phase"};
+
+    public static String buildDomain () {
+        String adjective = adjectives[(int) (Math.random()*10)];
+        String noun = nouns[(int) (Math.random()*10)];
+        return adjective + "-" + noun;
+    }
+    public static void main (String [] args) {
+        System.out.println();
+        System.out.println("your list of 10 possible domains.");
+        for (int i = 0; i<10; i++) {
+            System.out.println(buildDomain());
+        } //for loop close
+
+    } //main method close
+
+} // ServerNameGenerator class close
