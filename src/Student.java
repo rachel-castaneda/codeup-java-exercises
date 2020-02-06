a7b0f4207724af9ff99d5744e6c41c4208771edd
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-02-06 16:46:10 -0600

    worked on student

--- src/Student.java
+++ src/Student.java
@@ -4,10 +4,15 @@ public class Student {
 
     private String name;
     private ArrayList<Integer> grades;
+//    private int length = grades.size();
 
     public void addGrade(int grade){
         grades.add(grade);
     }
+
+//    public double getAverage (double grades) {
+//       return grades/length;
+//    }
 
     public Student (String name, ArrayList<Integer> grades) {
         this.name = name;
@@ -26,8 +31,25 @@ public class Student {
 
 //     main
     public static void main (String[] args) {
-        Student student1 = new Student ("john", new ArrayList<Integer>(){});
+        Student student1 = new Student ("bella swan", new ArrayList<Integer>(){});
         student1.addGrade(98);
-        System.out.println(student1.getGrades() +"\n"+ student1.getName());
+        student1.addGrade(65);
+        student1.addGrade(88);
+        System.out.println("student's name: " + student1.getName() +"\n"+ student1.getName()+"'s grades: " + student1.getGrades());
+        System.out.println("---------------------------------");
+
+        Student student2 = new Student ("edward cullen", new ArrayList<Integer>(){});
+        student2.addGrade(58);
+        student2.addGrade(65);
+        student2.addGrade(72);
+        System.out.println("student's name: " + student2.getName() +"\n"+ student2.getName()+"'s grades: " + student2.getGrades());
+        System.out.println("---------------------------------");
+
+        Student student3 = new Student ("jacob black", new ArrayList<Integer>(){});
+        student3.addGrade(100);
+        student3.addGrade(100);
+        student3.addGrade(100);
+        System.out.println("student's name: " + student3.getName() +"\n"+ student3.getName()+"'s grades: " + student3.getGrades());
+        System.out.println("---------------------------------");
     }
 }
