79631ca09302e54dd2ed86ec5eafed05e9b8b188
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-02-07 15:45:28 -0600

    worked on errors and exceptions

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -4,8 +4,14 @@ public class InputTest {
 
     public static void main (String [] args) {
         Input input = new Input();
-//        input.getDouble(77, 77.77);
-        System.out.println(input.yesNo());
+
+//        try {
+//            input.getInt("enter an integer: ");
+//        } catch (NumberFormatException nfe) {
+//            System.out.println("error");
+//        }
+        input.getDouble(77, 77.77);
+//        System.out.println(input.yesNo());
     }
 
 }
